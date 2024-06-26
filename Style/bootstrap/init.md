5f57f61c6e58b8ba1bfa87e56dee9cf1c7addb55
Joyeeeeeeeeeee <160221951+Joyeeeeeeeeeee@users.noreply.github.com>
2024-06-26 14:52:42 +0900

    부트스트랩

---
+++ Style/bootstrap/init.md
@@ -0,0 +1,11 @@
+# 프로젝트 생성
+npx create-react-app bootstrap
+
+# 라이브러리 설치
+
+## 기본 방식의 Bootstrap
+npm install bootstrap
+
+## 리액트 컴포넌트 기반의 Bootstrap
+- 📕 https://react-bootstrap.netlify.app/
+npm install react-bootstrap
---
+++ Style/bootstrap/src/components/ReactButton.jsx
@@ -0,0 +1,11 @@
+// import { Button } from 'react-bootstrap' // 아래꺼로 쓰거나 이거로 쓰거나 둘 다 되긴 함..
+import  Button  from 'react-bootstrap/Button'
+import React from 'react'
+
+const ReactButton = () => {
+  return (
+    <Button variant="primary">REACT Button</Button>
+  )
+}
+
+export default ReactButton
